421c4f82c5c69fc70a42becf482a00b02855e4ad
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-03-19 23:25:15 -0300

    Adicionado contexto

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import { ReactComponent as Halter } from '../Assets/imfit.svg';
+import { UserContext } from '../UserContext';
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to="/" aria-label="IMFit - Home">
           <Halter />
         </Link>
-        <Link className={styles.login} to="/login">
-          Login/Criar
-        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login/Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
