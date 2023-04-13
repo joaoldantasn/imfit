3f9ef87ad4dc04aba64a5417eee67b007af801db
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-04-13 16:18:50 -0300

    photo delete

--- src/api.js
+++ src/api.js
@@ -101,3 +101,15 @@ export function COMMENT_POST(id, body) {
     },
   };
 }
+
+export function PHOTO_DELETE(id) {
+  return {
+    url: API_URL + `api/foto/${id}`,
+    options: {
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer' + window.localStorage.getItem('token'),
+      },
+    },
+  };
+}
